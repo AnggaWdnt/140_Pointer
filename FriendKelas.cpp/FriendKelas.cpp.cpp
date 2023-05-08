0cd9237c0f2e3831967462c9e06b3ee68b8e8218
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-05-08 11:31:25 +0700

    tambahan void setid dan displayAll

--- FriendKelas.cpp/FriendKelas.cpp.cpp
+++ FriendKelas.cpp/FriendKelas.cpp.cpp
@@ -3,3 +3,18 @@
 using namespace std;
 
 class siswa;
+
+class orang {
+private:
+	string nama;
+public:
+	void setNama(string pNama);
+	friend class siswa;
+};
+class siswa {
+private:
+	int id;
+public:
+	void seId(int pId);
+	void displayAll(orang& a);
+};
